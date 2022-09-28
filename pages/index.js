ae33ce9ac14a2dcf465e35f02633289ae6ad845a
Benjamin <benjaminlopezyambot@gmail.com>
2022-09-28 14:46:58 +0700

    Initial

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 {/* TEST */}
 {/* TEST */}
+{/* TEST */}
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.j!s!</a>
