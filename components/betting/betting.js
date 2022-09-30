adae70c42174fb84fdc3a1e6b566a1befe759e63
benjamin <benjaminlopezyambot@gmail.com>
2022-09-30 11:02:57 +0700

    Added Betting Components

--- components/betting/betting.js
+++ components/betting/betting.js
@@ -1,65 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-// import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-import React, { useState } from "react";
-import Footer from '../components/common/footer';
-import Header from '../components/common/header';
-import HomeSlider from '../components/home/homeSlider';
-import Announcement from '../components/home/announcement';
-import PayoutSection from '../components/home/payoutSection';
-import GamePlayPrize from '../components/home/gamePlayPrize';
-import HowToPlay from '../components/home/howToPlay';
-import Banner from '../components/betting/banner';
-import Betting from '../components/betting/betting';
-export default function BettingNew() {
-  const [active, setActive] = useState(false)
-
-  return (
-    <>
-       <Head>
-          <title>Malaysia Lottery</title>
-          <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet"/>
-          <link href="assets/css/style.css" rel="stylesheet"/>
-          <link href="assets/css/owl.carousel.css" rel="stylesheet"/>
-          <link href="assets/css/owl.theme.default.css" rel="stylesheet"/>
-      </Head>
-      <Header/>
-      {/*--Mobile Menu--*/}
-      <div id="mySidepanel" className="sidepanel">
-          <a href="javascript:void(0)" className="closebtn" onclick="closeNav()">&times;</a>
-          <ul className="list-unstyled">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Home</a></li>
-          </ul>
-        </div>
-      {/*--Mobile Menu-*/}
- 
-
-      {/*-Breadcrumb--*/}
-{/* <section className="custom-breadcrumb">
-    <div className="container">
-        <div className="breadcrumb-heading">
-            <h1>BETTING</h1>
-        </div>
-        <div className="breadcrumb-list">
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li><span>Betting</span></li>
-            </ul>
-        </div>
-    </div>
-</section> */}
-    <Banner/>
-    <Betting/>
-{/*--Breadcrumb--*/}
-
-{/* <section className="page-content custom-padding">
+import Marquee from "react-fast-marquee";
+const Betting = () => {
+    return  (
+        <>
+            <section className="page-content custom-padding">
     <div className="container">
         <div className="row justify-content-center">
             <div className="col-md-3 col-sm-6">
@@ -570,14 +513,8 @@ export default function BettingNew() {
             </table>
         </div>
     </div>
-</section> */}
-  <Footer/>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/bootstrap/js/bootstrap.bundle.js"></script>
-<script src="assets/js/owl.carousel.js"></script>
-<script src="assets/js/main.js"></script>
-{/*--Footer--*/}
-</>
-      
-  )
+</section>
+        </>
+    )
 }
+export default Betting;
