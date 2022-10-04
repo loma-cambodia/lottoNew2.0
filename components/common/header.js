73dbe9a4119296c4a1bef58f0fc52dcdf070010e
Sushil Gupta <sushil.as.cambodia@gmail.com>
2022-10-04 11:06:39 +0700

    language setting

--- components/common/header.js
+++ components/common/header.js
@@ -3,51 +3,55 @@ import React, { useEffect, useState } from 'react';
 import i18n from '../../components/i18n';
 import Link from 'next/link'
 
-const changeLang = (l) => {
-  return () => {
-      // if (l == 'en') {
-
-      // }
-      // else if (l == 'de') {
-
-      // } else {
-
-      // }
-      i18n.changeLanguage(l);
-      localStorage.setItem('lang', l);
-  }
-}
-
-const changeLangm = (l) => {
-  return () => {
-    alert(l);
-      // if (l == 'en') {
-
-      // }
-      // else if (l == 'de') {
-
-      // } else {
-
-      // }
-      i18n.changeLanguage(l);
-      localStorage.setItem('lang', l);
-  }
-}
-
 const Header = ({datauser}) => {
-
+  const [langType, setLangType] = useState('');
   useEffect(() => {
     let currentLang = localStorage.getItem('lang');
     i18n.changeLanguage(currentLang);
-}, [])
+    setLangType(currentLang);
+  }, [langType])
+  
 
-useEffect(() => {
-    let currentLang = localStorage.getItem('lang');
-    i18n.changeLanguage(currentLang);
-}, [])
-
+  const changeLangm = (l) => {
+    return () => {
+        i18n.changeLanguage(l);
+        localStorage.setItem('lang', l);
+        setLangType(l);
+    }
+  }
   // const userDatas = JSON.parse(localStorage.getItem("name"));
-   console.log('Header:datauser:',datauser);
+  //  console.log('Header:datauser:',datauser);
+
+  function LangTypeFun({langTypeVal}){
+    // alert(langTypeVal)
+    if (langTypeVal == 'en') {
+      return(
+        <>
+            <span className="lang-flag">
+              <img src="assets/images/icons/flag-english.png"/>
+            </span> ENGLISH
+        </>
+      )
+    }
+    else if (langTypeVal == 'de') {
+      return(
+        <>
+            <span className="lang-flag">
+              <img src="assets/images/icons/flag-china.png"/>
+            </span> Chinese
+        </>
+      )
+    } else {
+      return(
+        <>
+            <span className="lang-flag">
+              <img src="assets/images/icons/flag-english.png"/>
+            </span> ENGLISH
+        </>
+      )
+    }
+  }
+
     return (
       <>
         <header className="header-top" data-spy="affix" data-offset-top="197">
@@ -80,17 +84,32 @@ useEffect(() => {
             <div className="right-part-menu">
                 <ul className="right-part-list">
                     <li>
-                        <span className="text-end mb-0 user-details"><span className="user-id text-black" >{datauser && datauser.user && datauser.user.data  && datauser.user.data.name ? datauser.user.data.name[0].toUpperCase() + datauser.user.data.name.substring(1)  : "" }</span><a href="#" className="reload-icon"><span ><img src="assets/images/icons/reload-white.png" alt="reload"/></span></a> <span className='text-black'>0.00</span> <span className="badge badge-yellow text-black">USD</span></span>
+                        <span className="text-end mb-0 user-details"><span className="user-id text-black" >{datauser && datauser.user && datauser.user.data  && datauser.user.data.name ? datauser.user.data.name[0].toUpperCase() + datauser.user.data.name.substring(1)  : "" }</span><a href="#" className="reload-icon"><span ><img src="assets/images/icons/reload-white.png" alt="reload"/></span></a> <span className='text-black'>0.00</span> <span className="badge badge-yellow text-black">{datauser && datauser.user && datauser.user.data  && datauser.user.data.currency_symbol ? datauser.user.data.currency_symbol  : "" }
+                         USD</span></span>
                     </li>
                     <li className="hide-650">
                         <a href="#" className="play-lottery-btn ">Play Lottery</a>
                     </li>
                     <li className="dropdown">
-                        <a href="#" className="lanugae-selector dropdown-toggle" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false"><span className="lang-flag"><img src="assets/images/icons/flag-english.png"/></span> ENG</a>
+                        <a href="#" className="lanugae-selector dropdown-toggle" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                          <LangTypeFun langTypeVal={langType} />
+                        </a>
                         <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
-                            <li><a className="dropdown-item" href="#">Action</a></li>
-                            <li onClick={changeLangm('en')}><a className="dropdown-item" href="#">English</a></li>
-                            <li onClick={changeLangm('de')}><a className="dropdown-item" href="#">Chinese</a></li>
+                            {/* <li><a className="dropdown-item" href="#">Action</a></li> */}
+                            <li onClick={changeLangm('en')}>
+                              <a className="dropdown-item" href="#">
+                                <span className="lang-flag">
+                                  <img src="assets/images/icons/flag-english.png"/>
+                                </span>&nbsp; &nbsp;English
+                              </a>
+                            </li>
+                            <li onClick={changeLangm('de')}>
+                              <a className="dropdown-item" href="#">
+                                <span className="lang-flag">
+                                  <img src="assets/images/icons/flag-china.png"/>
+                                </span>&nbsp; &nbsp;Chinese
+                              </a>
+                            </li>
                           </ul>
                     </li>
                     <li className="menu-mobile">
