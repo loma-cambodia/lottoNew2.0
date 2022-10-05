f1006dbd382dc52004107a83d9d5cb4661608237
benjamin <benjaminlopezyambot@gmail.com>
2022-10-05 11:39:31 +0700

    winnergame

--- components/home/winnerGameData.js
+++ components/home/winnerGameData.js
@@ -5,86 +5,89 @@ console.log("GAME--DATA:",data)
     const { t } = useTranslation();
 
     return (
-        <div className="row justify-content-center">
-        <div className="col-md-8">
-            <div className="coupon-block">
-                <div className="half-cicle left">
-
-                </div>
-                <div className="content-part">
-                    <div className="table-part">
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th className="text-end">{t('Big_Bet')}</th>
-                                    <th className="text-end">{t('Small_Bet')}</th>
-                                    <th className="text-end">3 A</th>
-                                    <th className="text-end">3 C</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>{t('1st_Prize')}</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                </tr>
-                                <tr>
-                                    <td>{t('2nd_Prize')}</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                </tr>
-                                <tr>
-                                    <td>{t('3rd_Prize')}</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                </tr>
-                                <tr>
-                                    <td>{t('Special_Prize')}</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                </tr>
-                                <tr>
-                                    <td>{t('Consolation_Prize')}</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                    <td className="text-end">5000</td>
-                                </tr>
-                            </tbody>
-                        </table>
+        
+            <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="coupon-block">
+                    <div className="half-cicle left">
+    
                     </div>
-                    <div className="payout-text-part">
-                        {t('PAYOUTS')}
+                    <div className="content-part">
+                        <div className="table-part">
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th className="text-end">{t('Big_Bet')}</th>
+                                        <th className="text-end">{t('Small_Bet')}</th>
+                                        <th className="text-end">3 A</th>
+                                        <th className="text-end">3 C</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{t('1st_Prize')}</td>
+                                        <td className="text-end">{data.big_first}</td>
+                                        <td className="text-end">{data.small_first}</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('2nd_Prize')}</td>
+                                        <td className="text-end">{data.big_second}</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('3rd_Prize')}</td>
+                                        <td className="text-end">{data.big_third}</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('Special_Prize')}</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{t('Consolation_Prize')}</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                        <td className="text-end">5000</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="payout-text-part">
+                            {t('PAYOUTS')}
+                        </div>
                     </div>
-                </div>
-                <div className="half-circles">
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
-                    <span className="circle-coupon"></span>
+                    <div className="half-circles">
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                        <span className="circle-coupon"></span>
+                    </div>
                 </div>
             </div>
+            
         </div>
-        
-    </div>
+       
+       
 
 
     );
