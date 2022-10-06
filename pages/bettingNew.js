e182f2b56a90dbcab6cd2a19e88e713a754826ca
Sushil Gupta <sushil.as.cambodia@gmail.com>
2022-10-06 09:53:18 +0700

    update h5

--- pages/bettingNew.js
+++ pages/bettingNew.js
@@ -6,8 +6,8 @@ import BettingOptionSelection from '../components/betting/bettingOptionSelection
 import {getBettingDates,lotterySubmit} from '../store/actions/bettingActions';
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 import BettingOptionSelectionForMob from '../components/betting/bettingOptionSelectionForMob';
+import styles from '../styles/Home.module.css'
 
 export default function BettingNew({datauser}) {
   const { t } = useTranslation();
@@ -32,22 +32,20 @@ export default function BettingNew({datauser}) {
           <link href="assets/text-fonts/poppins/poppins-font.css" rel="stylesheet" />
       </Head>
       <Header datauser={datauser}/>
-
-      <BrowserView>
-        <BettingOptionSelection _bettingDatesStore={bettingDatesStore}/> 
-      </BrowserView>
-     
-      <MobileView className="container">
-        <BettingOptionSelectionForMob _bettingDatesStore={bettingDatesStore}/>
-      </MobileView> 
-
-     <Footer/>
-      <script src="assets/js/jquery.min.js"></script>
-      <script src="assets/bootstrap/js/bootstrap.bundle.js"></script>
-      <script src="assets/js/owl.carousel.js"></script>
-      <script src="assets/js/main.js"></script>
-    {/*--Footer--*/}
-</>
-      
+        <div className={styles.device_detect_for_desktop}>
+          <BettingOptionSelection _bettingDatesStore={bettingDatesStore}/>
+        </div> 
+        <div className="container">
+            <div className={styles.device_detect_for_mobile}> 
+              <BettingOptionSelectionForMob _bettingDatesStore={bettingDatesStore}/>
+            </div>
+        </div>
+      <Footer/>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.bundle.js"></script>
+        <script src="assets/js/owl.carousel.js"></script>
+        <script src="assets/js/main.js"></script>
+      {/*--Footer--*/}
+    </>
   )
 }
