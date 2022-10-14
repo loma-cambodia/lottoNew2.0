d4530d2a11506be8453db6cadb0bbba55bc15ba0
benjamin <benjaminlopezyambot@gmail.com>
2022-10-14 16:28:05 +0700

    404 403

--- pages/403.js
+++ pages/403.js
@@ -9,66 +9,40 @@ export default function About403() {
     const { t } = useTranslation();
 
   return (
-    // <div className={`${styles.error} row`}>
-    <div className='row' style={{margin: '0',padding: '0',height:'100vh'}}>
+    <div className="container">
+    <div className='row align-items-center justify-content-center '>
         <Head>
-            <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet"/>
-          <link href="assets/css/style.css" rel="stylesheet"/>
-          <link href="assets/css/owl.carousel.css" rel="stylesheet"/>
-          <link href="assets/css/owl.theme.default.css" rel="stylesheet"/>
-        <title>Loma Lotto - 403 {t('access_denied')}</title>
+          <title>Loma Lotto - 403 Access Denied</title>
         </Head>
-        
-                                <div className="col-12 text-center" 
-                                style={{display: 'flex',justifyContent: 'center',alignItems: 'center'}}>
-                                    <img src="img/LOGO MALAYSIA BACK-01-02.png" style={{width:'150px',filter: 'drop-shadow(5px 10px 10px grey)'}} alt="" />
-                                </div>
-                            <div id=''>
-                                <div className="col-12 errorBg text-center" style={{}}>
-                                    <img src="img/error/403-animated-2.gif" style={{width:'35%'}} alt="" />
-                                </div>
-                                <div className="col-12" style={{display: 'flex',justifyContent: 'center',alignItems: 'center', flexDirection:'column',paddingTop:'100px'}}>      
-                                    <div className="section-title text-center border-bottom border-top border-4" style={{width:'60vw'}}>
-                                        <h1 className="sub-title py-3 smallLetter text-color-main" style={{}}>{t('access_denied_or_forbbiden')}</h1>
-                                    </div>
-                                        <br></br>
-                                    <div className='text-center'>
-                                      <span className="" style={{fontWeight: 'normal'}}>
-                                          {t('403_message')}
-                                      </span>
-                                    </div>
-                                  <div>
-                                      <a className="" style={{fontWeight: 'normal', color:'#fd5497'}}>
-                                          {t('return_to')} {t('Homepage')}.
-                                      </a>
-                                  </div>
-                                </div>
-                            </div>
 
-                        {/* <div className="error-page-menu">
-                                <ul>
-                                    <li>
-                                        <Link href="/">
-                                            <a className="single-menu">Homepage</a>
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/lotteries">
-                                            <a className="single-menu">Betting</a>
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/history">
-                                            <a className="single-menu">History</a>
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/result">
-                                            <a className="single-menu">Result</a>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div> */}
+                        <div className="col-md-6">
+                            <div className="text-center pt-3">
+                                <img src="img/LOGO MALAYSIA BACK-01.png" style={{width: '150px',height: 'auto',paddingBottom:'',paddingLeft:'0', margin:'0 auto'}} alt="" />
+                                <br></br>
+                                <img  src="img/error/403-animated-2.gif" style={{width: '100%'}}  alt="" />
+                                    <hr></hr>
+                                    <h3 className="fw-bold">Access Denied Or Forbidden</h3>
+                                    <h3 className="fw-bold">访问被拒绝或禁止</h3>
+                                    <h3 className="fw-bold">ដំណើរ​ការ​ត្រូវ​បាន​បដិសេធ</h3>
+                                    <hr></hr>
+                                    {/* <p className="fs-3"> <span className="text-danger">Opps!</span> Status 404</p> */}
+                                    <p className="lead">
+                                    The page or resource you’re trying to reach is absolutely forbidden for some reason.<br/>
+                                        <a href="/" style={{color: '#be2263'}}>Return to the Home Page </a>
+                                    </p>
+                                    
+                                    <p className="lead">
+                                    由于某种原因，您尝试访问的页面或资源被禁止 <br/>
+                                    <a href="/" style={{color: '#be2263'}}>返回主页</a>
+                                    </p>
+                                    
+                                    <p className="lead">
+                                       ទំទំព័រ ឬធនធានដែលអ្នកកំពុងព្យាយាមចូលប្រើត្រូវបានហាមឃាត់ដោយហេតុផលមួយចំនួន<br/><a href="/" style={{color: '#be2263'}}>ត្រឡប់ទៅទំព័រដើមវិញ</a>
+                                    </p>
+                                    
+                            </div>
+                        </div>
+    </div>
     </div>
   );
 }
