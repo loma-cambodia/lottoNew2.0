fd1431abecc38e6875b4d8493191c5ba3bf8cb7d
Dileep <dileepdileep45@gmail.com>
2022-10-18 12:56:29 +0700

    add

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import GamePlayPrize from '../components/home/gamePlayPrize';
 import HowToPlay from '../components/home/howToPlay';
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
-import {userTransactionDetails, winnerResultDetails2} from '../store/actions/homeActions';
+import {userTransactionDetails, winnerResultDetailsSecond} from '../store/actions/homeActions';
 import {getLogin} from '../store/actions/authActions';
 export default function Home({datauser, updateSessionData, setUpdateSessionData}) {
 
@@ -26,20 +26,15 @@ export default function Home({datauser, updateSessionData, setUpdateSessionData}
   //console.log('Home:datauser:',datauser);
 
 useEffect(() => {
-  dispatch(userTransactionDetails());
-  dispatch(winnerResultDetails2());
-}, [])
+  if(datauser && datauser.user && datauser.user.data && datauser.user.data.merchant_id)
+    dispatch(userTransactionDetails(datauser.user.data.merchant_id));
+  dispatch(winnerResultDetailsSecond());
+}, [datauser])
 
 
 
 useEffect(() => {
-  console.log('11111111111111');
-  console.log('datauser:',datauser);
 
-//   dispatch({
-//     type: "GET_LOGIN_DETAILS",
-//     payload: datauser && datauser.user && datauser.user.data ? datauser.user.data : {}
-// })
 let objectWithData = {  
   "customer_name": datauser && datauser.user && datauser.user.data && datauser.user.data.customer_name ? datauser.user.data.customer_name : '',
   "customer_id":  datauser && datauser.user && datauser.user.data && datauser.user.data.customer_id ? datauser.user.data.customer_id : 0,
