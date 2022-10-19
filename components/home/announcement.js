2ac103fc16d92a44e44093eb1346eb8c5f3dd94c
Sushil Gupta <sushil.as.cambodia@gmail.com>
2022-10-19 09:28:06 +0700

    update annoubcment

--- components/home/announcement.js
+++ components/home/announcement.js
@@ -3,56 +3,50 @@ import React from 'react';
 import { useTranslation } from "react-i18next";
 const Announcement = ({_announcementState, _language}) => {
     const { t } = useTranslation();
-  //  const userDatas = JSON.parse(localStorage.getItem("name"));
-    
     const announcementData = _announcementState
     let language =  _language;
-    console.log("announcementStateANNOUNCEMENT",announcementData)
-    
-    console.log('language',language)
+    function AnnouncementData({announcementDataNew}){
+      if(announcementDataNew.length > 0){
+        console.log('announcementStateANNOUNCEMENTSushil');
+        return (
+          <Marquee direction="left" pauseOnHover={true} speed={50}>
+            <ul className="list-inline">
+              {announcementDataNew.map((item, id) => {
+                    return(
+                        <li key={id} className="list-inline-item">{item.content[language]}</li>
+                    );
+                })}
+            </ul>
+          </Marquee> 
+        );
+      }
+    }
     return (
       <>
-     {/*--Announcement--*/}
-  <section className="announcement ">
-    <div className="container">
-        <div className="news-wrapper d-inline-flex align-item-center" id="myDIV">
-            <div className="announcement-block-icon">
-                <div className="annoncement-icon">
-                    <span className="icon-img-announcement"><img src="assets/images/icons/announcement-icon-white.png" alt="" className="img-fluid"/></span>
-                    <span class="text-announcement"><span class="announcement-text">{t('Announcement')}</span> <span class="toggle-icon-news" onClick={() => myFunction() } type="button">+</span></span>
-                </div>
-            </div>
-            <div className="marque-div">
-             <Marquee direction="left" pauseOnHover={true} speed={5}>
-                    <ul className="list-inline">
-                    <li> </li>
-                     {announcementData.map((item, id) => {
-                         return(
-                            
-                          <li key={id} className="list-inline-item">{item.content[language]}</li>
-                             );
-                      })}
-                    </ul>
-            </Marquee>       {/* <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li>
-                        <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li>
-                        <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li>
-                        <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li> */}
-                    
-            
-            
-                {/* <marquee behavior="scroll" direction="left" onmouseover="this.stop();" onmouseout="this.start();">
-                    <ul className="list-inline">
-                        <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li>
-                        <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li>
-                        <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li>
-                        <li className="list-inline-item">Go on... hover me (and hold the mouse over)!</li>
-                    </ul>
-                </marquee> */}
-            </div>
-        </div>
-    </div>
-  </section>
-  {/*--Announcement--*/}
+        <section className="announcement ">
+          <div className="container">
+              <div className="news-wrapper d-inline-flex align-item-center" id="myDIV">
+                  <div className="announcement-block-icon">
+                      <div className="annoncement-icon">
+                          <span className="icon-img-announcement">
+                            <img src="assets/images/icons/announcement-icon-white.png" alt="" className="img-fluid"/>
+                          </span>
+                          <span class="text-announcement">
+                            <span class="announcement-text">
+                              {t('Announcement')}
+                            </span> 
+                            <span class="toggle-icon-news" onClick={() => myFunction() } type="button">
+                              +
+                            </span>
+                          </span>
+                      </div>
+                  </div>
+                  <div className="marque-div">
+                      <AnnouncementData announcementDataNew={announcementData} />
+                  </div>
+              </div>
+          </div>
+        </section>
       </>
     )
   }
