f40cd32ef83c5c2e8a30b7e46596a49e903484a4
azazali186 <115604442+azazali186@users.noreply.github.com>
2022-10-21 12:29:29 +0700

    Calculator Added

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import {announcement, userTransactionDetails, winnerResultDetailsSecond, specialDraw} from '../store/actions/homeActions';
 import {getLogin} from '../store/actions/authActions';
+import Calculator from '../components/calculator/Calculator';
 export default function Home({datauser, updateSessionData, setUpdateSessionData}) {
 
 
@@ -74,6 +75,7 @@ if(objectWithData.customer_id != 0){
       <HomeSlider _specialDrawState={ specialDrawState} datauser={datauser} _auth={auth} updateSessionData={updateSessionData} setUpdateSessionData={setUpdateSessionData} />
       <Announcement _announcementState={ announcementState}  _language = { language }/>
       <PayoutSection _transactions={transactions}/>
+      <Calculator _transactions={transactions}/>
       <GamePlayPrize _winnerResultDetails ={winnerResultDetails}/>
       <HowToPlay/>
       
