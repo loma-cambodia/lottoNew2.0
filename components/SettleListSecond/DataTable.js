69872662b719b2ef6c55ec12dfaf332a92506164
vo1an <ngetvirakvoan@gmail.com>
2022-10-27 16:31:38 +0700

    style react data table

--- components/SettleListSecond/DataTable.js
+++ components/SettleListSecond/DataTable.js
@@ -25,13 +25,26 @@ const Table = props => {
         }
      };
                                  
-    const customStyles ={
-        rows: {
-            style: {
-                fontSize: '14px',
+            //  Internally, customStyles will deep merges your customStyles with the default styling.
+        const customStyles = {
+            rows: {
+                style: {
+                    minHeight: '72px', // override the row height
+                },
             },
-        },
-    }
+            headCells: {
+                style: {
+                    paddingLeft: '8px', // override the cell padding for head cells
+                    paddingRight: '8px',
+                },
+            },
+            cells: {
+                style: {
+                    paddingLeft: '8px', // override the cell padding for data cells
+                    paddingRight: '8px',
+                },
+            },
+        };
     const columns = [
         {
             name: t('No'),
