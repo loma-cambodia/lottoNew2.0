11e7bd327adf1c2c22e8c784fc6a60ec5c090985
azazali186 <115604442+azazali186@users.noreply.github.com>
2022-11-03 12:26:43 +0700

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ export const getServerSideProps = withIronSessionSsr(
     password: "complex_password_at_least_32_characters_long",
     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
     },
   },
 );
